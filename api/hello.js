8466d7f402f19a95100b0aef657a580719f10cc8
sbabachev <st.babach@gmail.com>
2025-12-15 23:01:21 +0200

    test1

--- api/hello.js
+++ api/hello.js
@@ -1,3 +1,3 @@
-export default function handler(req, res) {
-  res.status(200).json({ message: "Hello World from Vercel Test by Bilbo Baggins!" });
-}
+module.exports = (req, res) => {
+  res.status(200).send("OK");
+};
